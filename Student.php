858c7d6f253f968dbadcfbdf7f55f0f2321a08df
derek_larson@bcit.ca <derek.braden.larson@gmail.com>
2016-09-08 17:18:42 -0700

    Added student 1 and 2. Also put average method into student.php

--- Student.php
+++ Student.php
@@ -28,6 +28,14 @@ class Student {
         $this->grade[] = $grade;
     }
     
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value) {
+            $total += $value;
+        }
+        return $total / count($this->grades);
+        } 
+
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
